ce0228b65ccfb92c0417dfa44993c0da435160d0
cypheriumlite <cypheriumlite@gmail.com>
2021-02-15 18:02:53 +0000

    Change of 'magic number' in pch message to a unique value

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -101,10 +101,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xc0;
-        pchMessageStart[2] = 0xb6;
-        pchMessageStart[3] = 0xdb;
+        pchMessageStart[0] = 0xbb;
+        pchMessageStart[1] = 0xce;
+        pchMessageStart[2] = 0xb9;
+        pchMessageStart[3] = 0xd4;
         nDefaultPort = 9333;
         nPruneAfterHeight = 100000;
         m_assumed_blockchain_size = 22;
